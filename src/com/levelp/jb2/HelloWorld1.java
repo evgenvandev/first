3135ca180a9facb594c4b38a69c3bfb35f21d452
evgenvandev <developjev@gmail.com>
2019-02-10 12:17:36 +0500

    create HelloWorld.java in package com.levelp.jb2

---
+++ src/com/levelp/jb2/HelloWorld1.java
@@ -0,0 +1,10 @@
+package com.levelp.jb2;
+
+/**
+ * Created by Администратор on 10.02.2019.
+ */
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
